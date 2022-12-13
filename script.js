7b319cc16b4cf6b3996fe9c085067204bf83327f
DestinyFrog <petrocalixto5@hotmail.com>
2022-12-13 15:21:58 -0300

    Update Calculus

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
+const calculus = {
+    ["+"]: (x,y) => {return x + y;},
+    ["-"]: (x,y) => {return x - y;},
+    ["*"]: (x,y) => {return x * y;},
+    ["÷"]: (x,y) => {return (x / y).toFixed(3);},
+    ["^"]: (x,y) => {return Math.pow(x,y);}}
 function Generate() {
     let max = parseInt(document.getElementById('max').value);
     let opera = document.getElementById('opera').value;
     let x = rand(max-1)+1;
     let y = rand(max-1)+1;
-    return (' '+x+' '+opera+' '+y+' ');
+    return ( x+ " " +opera+ " " +y );
 } function rand(maxi) {return Math.floor(Math.random()*maxi);}
 function createBox() {
     let liz = document.createElement('li');
-    let intex = document.createElement('a');
+    let intex = document.createElement('p');
     let val = Generate();
     intex.appendChild( document.createTextNode(val) );
 
     let butExc = document.createElement('button');
-    butExc.innerText = "🗑️";
+    butExc.innerText = "🙅";
+    butExc.style = "background-color: #222233;font-size: 22.5px;"
     butExc.addEventListener('click', (obj) => {
         obj.target.parentElement.remove();
     });
@@ -21,12 +28,10 @@ function createBox() {
     but.setAttribute('type', 'button');
     but.innerText = ' = ';
     but.addEventListener('click', (obj) => {
+        var pie = val.split(" ");
+        console.log( pie );
         if(obj.target.innerText == "="){
-            let result = eval( val );
-            if( Number.isInteger(result) == false){
-                result = result.toFixed(3); }
-
-            obj.target.innerText = '= '+ result;
+            obj.target.innerText = '= '+ calculus[pie[1]]( parseInt(pie[0]),parseInt(pie[2]) );
         }else{
             obj.target.innerText = '=';
         }
